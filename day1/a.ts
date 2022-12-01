01da189ad6effbee57c02c015c0b11f120a6826b
Rundez <m.ruud93@gmail.com>
2022-12-01 23:52:58 +0100

    stuff

--- day1/a.ts
+++ day1/a.ts
@@ -2,23 +2,22 @@ const fs = require("fs");
 
 const data = fs.readFileSync("./input.txt", "utf8").split("\n");
 
-// let highest = 0;
-// let curr = 0;
-// for (let i = 0; i < data.length; i++) {
-//   if (data[i] === "") {
-//     if (curr > highest) {
-//       highest = curr;
-//     }
-//     curr = 0;
-//     continue;
-//   }
-//   curr += parseInt(data[i]);
-// }
+let highest = 0;
+let curr = 0;
+for (let i = 0; i < data.length; i++) {
+  if (data[i] === "") {
+    if (curr > highest) {
+      highest = curr;
+    }
+    curr = 0;
+    continue;
+  }
+  curr += parseInt(data[i]);
+}
 
 
 const elves: number[] = [];
 
-let curr = 0;
 for (let i = 0; i < data.length; i++) {
   if (data[i] === "") {
     elves.push(curr);
